4fa64e9020a0e8a63132de16dc995f4801965d4b
pabloitto <juanitto_40@hotmail.com>
2017-02-15 10:36:37 -0700

    Remove unused control

--- src/components/main.ts
+++ src/components/main.ts
@@ -2,14 +2,12 @@ import { Todo } from './../models/todo';
 import { TodoListComponent } from './todo-list-component';
 export class Main {
 
-    private _addButton: HTMLButtonElement;
     private _inputTodo: HTMLInputElement;
     private _todoListComponent: TodoListComponent;
     private _todoForm: HTMLFormElement;
 
     public init(): void {
         this._todoListComponent = new TodoListComponent("todo-list");
-        this._addButton = <HTMLButtonElement>document.getElementById("add");
         this._inputTodo = <HTMLInputElement>document.getElementById("input-todo");
         this._todoForm = <HTMLFormElement>document.getElementById("todo-form");
         this._todoForm.addEventListener("submit", (event) => {
